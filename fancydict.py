f85af8aba5e08e30be44bb8bc647f48f5605c1b7
Saska Karsi <saska.karsi@gmail.com>
2020-03-04 20:18:35 +0200

    Adding a couple of helper methods, docstrings

--- fancydict.py
+++ fancydict.py
@@ -45,3 +45,48 @@ class FancyDict(UserDict):
     def map_leaves(self, callable_):
         for k, v in self.traverse():
             self.__setitem__(k, callable_(v))
+
+    def get_by_list(self, keys):
+        """
+        Note that simple lists of indenting keys
+        can be fed straight to __getitem__, this is a 
+        helper method to try and find any value in a list of keys.
+        Args:
+            keys:
+                list of str or list
+        """
+        for k in keys:
+            try:
+                return self.__getitem__(k)
+            except KeyError:
+                continue
+        if self.strict_get:
+            raise KeyError(f'No key in {keys} found')
+        return None
+
+    def map_with_mapper_dict(self, mapper_dict):
+        """
+        Since this seems useless-ish I'm going to defend myself.
+        You want to harmonize data from multiple sources with
+            different structures but you know the path to the same
+            data point in each one. You can create a mapper dict
+            for them and just throw whatever documents you receive into 
+            a fancydict, then map them to the same format with this method.
+            If you get a new data source, add stuff to your mapping dict.
+            (Making sure they don't have different data points in the same path obv)
+        Args:
+            mapper_dict:
+                {
+                    key: [mappings],
+                    key2: [more mappings]
+                }
+        returns:
+            {
+                key: value from self if any mapping matched
+            }
+
+        Useful when:
+            - 
+        """
+        ret_d = {}
+        return {k: self.get_by_list(v) for k, v in mapper_dict.items()}
