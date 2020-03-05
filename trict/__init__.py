ac4bf33923877237032362eaafdb0b85b5d04008
Saska Karsi <saska.karsi@gmail.com>
2020-03-05 19:37:47 +0200

    Add leaves to __init__

--- trict/__init__.py
+++ trict/__init__.py
@@ -1,3 +1,3 @@
 from trict.trict import Trict
 from trict.util import (flatten_dict, iter_keys, recursive_delete,
-                        recursive_set, traverse)
+                        recursive_set, traverse, leaves)
