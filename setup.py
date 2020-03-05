e171f3f87688a44f4f90698b68de8279b34e011b
Saska Karsi <saska.karsi@gmail.com>
2020-03-05 11:16:44 +0200

    Add setup files

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+from distutils.core import setup
+setup(
+  name = 'trict',
+  packages = ['trict'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'UserDict subclass with extra stuff',
+  author = 'Saska Karsi',
+  author_email = 'saska.karsi@gmail.com',
+  url = 'https://github.com/saskakarsi/trict',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['mapping', 'map', 'dictionary', 'recursive', 'dict'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
