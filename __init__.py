490e4620635f5402262dd408694d95e262faa279
Saska Karsi <saska.karsi@gmail.com>
2020-03-05 11:06:18 +0200

    Move init, change imports

--- __init__.py
+++
@@ -1,2 +0,0 @@
-from trict.trict import Trict
-from trict.util import recursive_set, recursive_delete, flatten_dict, iter_keys, traverse
---
+++ trict/__init__.py
@@ -0,0 +1,3 @@
+from trict import Trict
+from util import (flatten_dict, iter_keys, recursive_delete,
+                        recursive_set, traverse)
